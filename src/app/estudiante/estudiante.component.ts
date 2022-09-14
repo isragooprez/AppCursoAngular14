82a55b71704a8ce86e0c8a5d6314e3c96e358d2c
Santiago Perez <santiago.perez@easysoft.com.ec>
2022-09-14 08:10:47 -0500

    Issue1: Add Component estudiantes con ng g c estudiantes comandos angular utils

---
+++ src/app/estudiante/estudiante.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estudiante',
+  templateUrl: './estudiante.component.html',
+  // template: '<p>estudiante works!</p><app-personas></app-personas>',
+  styleUrls: ['./estudiante.component.css']
+})
+export class EstudianteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
