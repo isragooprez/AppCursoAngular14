6cee2e2cac7987a7864661d4cab51637f97333bd
ISRAGOO .PREZ <isragoo.prez@gmail.com>
2022-09-14 22:02:45 -0500

    issue1: add Modulos Binding Agregar Persona

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -1,15 +1,20 @@
-import { Component } from "@angular/core";
+import {Component} from "@angular/core";
 
 @Component({
-  selector:'app-personas',
-  templateUrl:'./personas.component.html',
-   styleUrls:['./personas.component.css']
+  selector: 'app-personas',
+  templateUrl: './personas.component.html',
+  styleUrls: ['./personas.component.css']
 })
 
-export class PersonasComponent{
+export class PersonasComponent {
 
-  title='Personas';
-  titulo= 'MUDULO PERSONAS';
-  listaPersonasTitle='LISTA DE PERSONAS';
+  title = 'Personas';
+  titulo = 'MUDULO PERSONAS';
+  listaPersonasTitle = 'LISTA DE PERSONAS';
+  deshabilitar = false;
+  mensaje = 'No se ha agregado ninguna persona';
 
+  agregarPersona() {
+    this.mensaje = 'Persona agregada';
+  }
 }
