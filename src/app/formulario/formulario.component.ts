b7c9b578ca110c0652905545f6ec526fa061835c
Santiago Perez <santiago.perez@easysoft.com.ec>
2022-10-04 09:37:01 -0500

    Issue2: Agregar //Two-way binding // Local References #apellidoInput // @ViewChild('nombreInput') nombreInput: ElementRef;

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import {Person} from "../persona/person.model";
 
 @Component({
@@ -12,18 +12,54 @@ export class FormularioComponent implements OnInit {
   }
 
   @Output() personaCreada = new EventEmitter<Person>();
+  @ViewChild('nombreInput') nombreInput: ElementRef;
+  @ViewChild('apellidoInput') apellidoInput: ElementRef;
 
-  nombreInput: string = '';
-  apellidoInput: string = '';
 
   ngOnInit(): void {
   }
 
-
   createPersona() {
-    let persona1 = new Person(this.nombreInput, this.apellidoInput);
+    let persona1 = new Person(this.nombreInput.nativeElement.value, this.apellidoInput.nativeElement.value);
     //this.people.push( persona1 );
     this.personaCreada.emit(persona1);
   }
+
+
+  //PRIMERA FORMA DE RECUPERA DATOS VISTA
+  //Two-way binding
+//   Two-way binding gives components in your application a way to share data. Use two-way binding to listen for events and update values simultaneously between parent and child components.
+//   See the live example / download example for a working example containing the code snippets in this guide.
+// Prerequisites
+//   To get the most out of two-way binding, you should have a basic understanding of the following concepts:
+//   Property binding
+//   Event binding
+//   Inputs and Outputs
+//
+//   nombreInput: string = '';
+//   apellidoInput: string = '';
+
+  // createPersona() {
+  //   let persona1 = new Person(this.nombreInput, this.apellidoInput);
+  //   this.personaCreada.emit(persona1);
+  // }
+
+  //SEGUNDA FORMA DE RECUPERA DATOS VISTA
+  // <!--  Local References #apellidoInput-->
+  // createPersona(nombreInput: HTMLInputElement, apellidoInput:HTMLInputElement) {
+  //   let persona1 = new Person(nombreInput.value, apellidoInput.value);
+  //   this.personaCreada.emit(persona1);
+  // }
+
+  // TERCERA FORMA DE RECUPERAR LOS DATOS VIEWCHAILD
+  // @ViewChild('nombreInput') nombreInput: ElementRef;
+  // @ViewChild('apellidoInput') apellidoInput: ElementRef;
+  // createPersona() {
+  //   let persona1 = new Person(this.nombreInput.nativeElement, this.apellidoInput.nativeElement);
+  //   this.personaCreada.emit(persona1);
+  // }
+
+
+
 
 }
